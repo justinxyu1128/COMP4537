1913c56438a6008e85e0bda54c1b56524ad06f6a
justinxyu1128 <justinxyu1128@gmail.com>
2026-09-12 22:56:38 -0700

    added routing

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const comp4537Router = express.Router();
+const labsRouter = express.Router();
+
+app.get('/', (req, res) => {
+    res.redirect('/COMP4537');
+})
+
+comp4537Router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'COMP4537', 'index.html'));
+})
+
+labsRouter.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'COMP4537', 'labs', 'index.html'));
+})
+
+labsRouter.get('/1', (req, res) => {
+    res.sendFile(path.join(__dirname, 'COMP4537', 'labs', '1', 'index.html'));
+});
+
+
+app.use('/COMP4537', comp4537Router);
+comp4537Router.use('/labs', labsRouter);
+
+let port = 10000;
+app.listen(port, function () {
+    console.log("Server listening on port " + port);
+});
